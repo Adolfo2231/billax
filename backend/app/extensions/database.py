8c7596ff1d2e9541e7e2e197fdc41a098d39fe00
Adolfo <adolfo.rodriguez2231@gmail.com>
2025-06-18 20:39:58 -0400

    feat: add SQLAlchemy database extension setup

--- backend/app/extensions/database.py
+++ backend/app/extensions/database.py
@@ -1,5 +1,3 @@
-# Database extension placeholder
-# Will be implemented when we add SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-# from flask_sqlalchemy import SQLAlchemy
-# db = SQLAlchemy() 
+db = SQLAlchemy()
