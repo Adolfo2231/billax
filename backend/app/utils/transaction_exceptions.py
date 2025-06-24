61cd4227f99e332c8a554ef786c171a346927ea0
Adolfo <adolfo.rodriguez2231@gmail.com>
2025-06-23 23:26:18 -0400

    feat: add custom exceptions for transaction operations (TransactionNotFoundError, TransactionTypeNotFoundError)

--- backend/app/utils/transaction_exceptions.py
+++ backend/app/utils/transaction_exceptions.py
@@ -3,3 +3,9 @@ class TransactionNotFoundError(Exception):
     def __init__(self, message: str = "Transaction not found"):
         self.message = message
         super().__init__(self.message)
+
+class TransactionTypeNotFoundError(Exception):
+    """Exception raised when a transaction type is not found."""
+    def __init__(self, message: str = "Transaction type not found"):
+        self.message = message
+        super().__init__(self.message)
